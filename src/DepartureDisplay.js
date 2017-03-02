0d1e47d9f4c1a780390c6032cd9d68307ea1d5a4
Joel Sullivan <joel@jsullivan.us>
2017-03-02 16:43:47 -0500

    Works on local system, not particularly pretty

--- src/DepartureDisplay.js
+++ src/DepartureDisplay.js
@@ -8,38 +8,46 @@ class DepartureDisplay extends PureComponent {
 
   constructor(props) {
     super(props)
-    this.state = {
-      data: null,
-      sortBy: null, // rowIdx
-      descending: false,
-    }
+    this.state = {dstate: null};  // null or DepartureState object
 
-    this.POLL_INTERVAL = 1000;  // milliseconds
+    this.POLL_INTERVAL = 10 * 1000;  // 10 seconds
 
     // Start polling at (approximately) set intervals.
-    this.pollDisplatData();
+    this.pollDisplayData();
     setInterval(() => this.pollDisplayData(), this.POLL_INTERVAL);
   }
 
   pollDisplayData() {
-    Papa.parse("http://developer.mbta.com/lib/gtrtfs/Departures.csv", {
+    Papa.parse("sample-departures.csv", {
       delimiter: ",",
       header: true,
-      dynamicTyping: true,
       download: true,
     	complete: results => {
         var newState = this.convertCsvToState(results);
-    		this.setState(newState);
+    		this.setState({dstate: newState});
     	}
     });
   }
 
+  getCsvUrl() {
+    if (window.location.hostname === "localhost") {
+      return "sample-departures.csv";
+    } else {
+      return "departures.php";
+    }
+  }
+
   convertCsvToState(results) {
     if (results.errors) {
-      results.errors.forEach(error => console.error(error));
+      results.errors.forEach(error => {
+        if (error.code !== "TooFewFields") {
+          console.error(error)
+        }
+      });
     }
     if (results.data) {
-      return new DepartureState(results.data);
+      var data = results.data.filter(item => item.TimeStamp !== "");
+      return new DepartureState(data);
     } else {
       return null;
     }
@@ -54,50 +62,11 @@ class DepartureDisplay extends PureComponent {
   }
 
   _renderTable() {
-    if (this.state.data === null) {
-      return <div className="loading">Loading...</div>
+    if (this.state.dstate === null) {
+      return <div className="loading">Waiting for response...</div>
+    } else {
+      return this.state.dstate.renderTable();
     }
-    return (
-      <table>
-        <thead>
-          <tr>{
-            this.props.headers.map((title, idx) => {
-              let header = title;
-              if (this.state.sortBy === idx) {
-                header += this.state.descending ? ' \u2191' : ' \u2193';
-              }
-              return (
-                <th
-                  key={idx}
-                  onClick={this._sortToggle.bind(this, idx)}
-                >
-                  {header}
-                </th>
-              )
-            })
-          }
-
-          </tr>
-        </thead>
-        <tbody>{
-          this.state.data.map((row, rowIdx) =>
-            <tr key={rowIdx}>{
-              row.map((cell, idx) => {
-                let contents = cell;
-                return (
-                  <td
-                    key={idx}
-                    data-row={rowIdx}
-                  >
-                    {contents}
-                  </td>
-                )
-              })
-            }</tr>
-          )
-        }</tbody>
-      </table>
-    )
   }
 
 }
--- src/DepartureState.js
+++ src/DepartureState.js
@@ -1,7 +1,58 @@
+import React from 'react';
+
 class DepartureState {
 
   constructor(data) {
-    // TODO
+    this.headers = ["Origin", "Time", "Destination", "Train#", "Track#", "Status"];
+    this.data = data;
+  }
+
+  renderTable() {
+    return (
+      <table>
+        <thead>
+          {this._renderHeaderRow()}
+        </thead>
+        <tbody>
+          {this.data.map((row) => this._renderRow(row))}
+        </tbody>
+      </table>
+    )
+  }
+
+  _renderHeaderRow() {
+    return (
+      <tr key="header">
+        {this.headers.map(name => <th key={name}>{name}</th>)}
+      </tr>
+    );
+  }
+
+  _renderRow(row) {
+    return (
+      <tr key={this._getRowKey(row)}>
+        <td key="Origin">{row.Origin}</td>
+        <td key="Time" className="time">
+          {this._formatTime(row.ScheduledTime)}
+        </td>
+        <td key="Destination">{row.Destination}</td>
+        <td key="Train">{row.Trip}</td>
+        <td key="Track">{row.Track}</td>
+        <td key="Status">{row.Status}</td>
+      </tr>
+    );
+  }
+
+  _getRowKey(row) {
+    return row.Destination + row.ScheduledTime;
+  }
+
+  _formatTime(time) {
+    var t = (+time) * 1000;  // convert to milliseconds
+    var d = new Date(t);
+    var s = d.toLocaleTimeString();
+    var splitArray = s.split(":00 ");
+    return splitArray[0] + " " + splitArray[1];
   }
 };
 
